bca45968c6bad818c1f86fd25c60fb24e9d51090
Ram Charan <25ec01028@iitbbs.ac.in>
2026-08-07 11:03:25 +0530

    added roll no

--- check_setup.py
+++ check_setup.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-print("Python version verified", sys.version.split()[0])
-print("numpy :", np. version )
-print("pandas :", pd. version )
-print("matplotlib:", matplotlib. version )
+print("Python :", sys.version.split()[0])
+print("Numpy :", np.__version__)
+print("pandas :", pd.__version__)
+print("matplotlib:", matplotlib.__version__)
 # a one-line smoke test of the plotting back-end
 plt.plot([0, 1, 2, 3], [0, 1, 4, 9], marker="o")
 plt.title("If you can see this window, the setup works")
-plt.xlabel("x"); plt.ylabel("x squared")
+plt.xlabel("x")
+plt.ylabel("x squared")
 plt.grid(True)
 plt.show()
+print("roll no: 25EC01028")
